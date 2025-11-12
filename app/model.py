897bccdb1b957d756d3f399a1c173b5cb501ce02
Adriano <adriano.ravina@deeptheagency.com>
2025-11-12 10:15:29 +0100

    Initial commit

---
+++ app/model.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, Field, EmailStr
+
+class PostSchema(BaseModel):
+    id: int = Field(default=None)
+    title: str = Field(...)
+    content: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Securing FastAPI applications with JWT.",
+                "content": "How secure your application by enabling authentication using JWT."
+            }
+        }
+
+class UserSchema(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullname": "John Smith",
+                "email": "smith@x.com",
+                "password": "weakpassword"
+            }
+        }
+
+class UserLoginSchema(BaseModel):
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "smith@x.com",
+                "password": "weakpassword"
+            }
+    }
